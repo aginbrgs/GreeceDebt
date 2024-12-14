bb5c774b0854f0a0b1de2e413bf864d052bb713b
aginbrgs <christoph@ebruenner.de>
2024-12-14 17:44:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ file_path = r"YourDataPath.csv" # Enter your Path here
 df  = pd.read_csv(file_path)
 
 
-                                                                #Here you should introduce your crash date
+#Here you should introduce your crash date
 df_before2010=df[df['TIME_PERIOD']<2010]
 df_after2010=df[df['TIME_PERIOD']>=2010]
-df['Pre_Crisis'] = (df['TIME_PERIOD'] <= 2011).astype(int)      #Splitting the data at the crashdate and ensuring its int
+df['Pre_Crisis'] = (df['TIME_PERIOD'] <= 2011).astype(int)      
 
 y = df_before2010['Interest in % GDP']
 X = df_before2010[['Growth','Tax Revenue in % GDP','Social Expenditure in % of GDP','dummy']]
 X = sm.add_constant(X)
-                                                                #Defining target variable and predictors
+#Defining target variable and predictors
 X_clean = X.replace([np.inf,-np.inf,np.nan])
 X_clean = X_clean.dropna()
 y_clean = y[X_clean.index]
 
-
-ols_model = sm.OLS(y_clean, X_clean).fit()                      #Fitting OLS
-
-Autocor = sm.tsa.acf(ols_model.resid,fft=False,nlags=1)[1] #Calculate the autocorrelation in time series with a lag of 1
-y_transformed = y_clean.diff().dropna() - Autocor * y_clean.shift(1).dropna() #De autocorrelated version of original time series 
+#Fitting OLS
+ols_model = sm.OLS(y_clean, X_clean).fit()
+#De autocorrelated version of original time series
+Autocor = sm.tsa.acf(ols_model.resid,fft=False,nlags=1)[1] 
+y_transformed = y_clean.diff().dropna() - Autocor * y_clean.shift(1).dropna()  
 X_transformed = X_clean.diff().dropna() - Autocor * X_clean.shift(1).dropna()
-
+#Output
 gls = sm.GLS(y_transformed,X_transformed).fit() #Fitting GLS
 print(gls.summary())
 
